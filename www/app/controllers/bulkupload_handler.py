41a82282a8ddc7dc0abf43c9a735421948f2bb0f
Samuel Sekiwere <sekiskylink@gmail.com>
2018-03-13 18:01:37 +0300

    use fine-uploader for the bulkuploads of reporters

--- www/app/controllers/bulkupload_handler.py
+++ www/app/controllers/bulkupload_handler.py
@@ -1,7 +1,8 @@
 import web
 import os
 import tempfile
-from . import require_login, render, get_session
+import json
+from . import db, require_login, render, get_session
 from settings import BASE_DIR
 UPLOAD_SCRIPT = BASE_DIR + "/load_vhtdata.py"
 
@@ -19,28 +20,45 @@ class BulkUpload:
 
     @require_login
     def POST(self):
-        params = web.input(uploadfile={})
-        session = get_session()
+        params = web.input(uploadfile={}, caller='web', user='api_user')
+        # print "===>", params.keys()
+        # print params.qquuid
+        # print params.qqtotalfilesize
+        # print params.qqfilename
+        if params.caller != 'api':
+            session = get_session()
+            username = session.username
+            # userid = session.sesid
+            role = session.role
+        else:
+            rs = db.query(
+                "SELECT a.id, a.username, b.name as role "
+                "FROM users a, user_roles b "
+                "WHERE a.username = '%s' AND a.user_role = b.id" % params.user)
+            if rs:
+                user = rs[0]
+                # userid = user['id']
+                username = user['username']
+                role = user['role']
 
-        if session.role == 'District User':
-            district = session.username.capitalize()
-            user = session.username
-            if params.uploadfile.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-                # print dir(params.uploadfile)
-                f = tempfile.NamedTemporaryFile(suffix=".xlsx", delete=False)
-                # print f.name
-                f.write(params.uploadfile.file.read())
-                cmd = UPLOAD_SCRIPT + " -f %s -d %s -u %s" % (f.name, district, user)
-                try:
-                    f.close()
-                except:
-                    pass
-                # print cmd
-                os.popen(cmd)
-                os.unlink(f.name)
-            else:
-                sess_err = "Upload file should be an Excel spreadsheet."
-
-        l = locals()
-        del l['self']
-        return render.bulkupload(**l)
+        if role == 'District User':
+            district = username.capitalize()
+            f = tempfile.NamedTemporaryFile(suffix=".xlsx", delete=False)
+            print f.name
+            f.write(params.qqfile)
+            cmd = UPLOAD_SCRIPT + " -f %s -d %s -u %s" % (f.name, district, username)
+            try:
+                f.close()
+            except:
+                pass
+            print cmd
+            os.popen(cmd)
+            os.unlink(f.name)
+            if params.caller == 'api':
+                return json.dumps({'success': 'true', 'message': 'queued excel sheet for upload'})
+            return json.dumps({'success': True})
+        else:
+            return json.dumps({
+                "success": False,
+                "error": "District login required for bulk upload", "preventRetry": True})
+        return json.dumps({"success": False, "error": "error message to display", "preventRetry": True})
