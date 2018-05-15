2a5f0332ea1de5be5d0844a014bb2de1235e6ecb
Samuel Sekiwere <sekiskylink@gmail.com>
2018-05-15 15:26:06 +0300

    fix deployment fabfile

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -1,14 +1,14 @@
 from fabric.api import local, abort, run, lcd, cd, settings, sudo, env
 
-env.hosts = ['llinweb']
+env.hosts = ['fc.gcinnovate.com']
 env.use_ssh_config = True
 
 
 def deploy():
-    local_code_dir = '/Users/sam/projects/mtrackpro'
+    local_code_dir = '/Users/sam/projects/fconnect'
     with lcd(local_code_dir):
         local("git push origin master")
-    code_dir = '/var/www/mtrackpro/web'
+    code_dir = '/var/www/vhtportal/fconnect'
     with cd(code_dir):
         run("git pull origin master")
-        sudo("supervisorctl restart mtrackpro")
+        sudo("supervisorctl restart vhtportal")
